98f6ed7c6cfe67ff423a8fa1e3c5b2d7eb04c1aa
bn4t <me@bn4t.me>
2020-09-24 11:33:57 +0200

    Add tailwind and restructure the project a bit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [
+	"./src/**/*.html",
+	"./src/**/*.vue",
+	"./src/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
