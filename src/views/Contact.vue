e3f233979d6c87a842273f310963fc58cdb1aafb
Benjamin Nater <17193640+bn4t@users.noreply.github.com>
2021-06-20 19:08:55 +0200

    Update Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,7 +2,7 @@
   <div class="text-center">
     <h1 class="text-5xl mb-2">Contact</h1>
     <p class="mt-6 mb-2">Interested in hiring me?</p>
-    <p>Feel free to write me an email at <a class="underline" href="mailto:benjamin@nater.net">benjamin@nater.net</a>, text me on matrix at <a class="underline" href="https://matrix.to/#/@bn4t:matrix.bn4t.me">@bn4t:matrix.bn4t.me</a> or
+    <p>Feel free to write me an email at <a class="underline" href="mailto:benjamin@nater.net">benjamin@nater.net</a> or
       contact me through <a class="underline" href="https://www.linkedin.com/in/benjamin-nater/">LinkedIn</a>.
     </p>
   </div>
